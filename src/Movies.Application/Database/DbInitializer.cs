564712554e8a79ed12f4b36ce8c892ea8340c73a
jd-HW <johannes.dereuddre@student.howest.be>
2025-06-01 12:08:49 +0200

    Add database connection factory and schema initializer

---
+++ src/Movies.Application/Database/DbInitializer.cs
@@ -0,0 +1,26 @@
+using Dapper;
+
+namespace Movies.Application.Database;
+
+public class DbInitializer(IDbConnectionFactory dbConnectionFacory)
+{
+    private readonly IDbConnectionFactory _dbConnectionFacory = dbConnectionFacory;
+
+    public async Task InitializeAsync()
+    {
+        using var connection = await _dbConnectionFacory.CreateConnectionAsync();
+
+        await connection.ExecuteAsync("""
+            CREATE TABLE IF NOT EXISTS movies (
+            id UUID PRIMARY KEY,
+            slug TEXT NOT NULL,
+            title TEXT NOT NULL,
+            year_of_release INTEGER NOT NULL);
+        """);
+
+        await connection.ExecuteAsync("""
+            CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS movies_slug_idx ON movies
+            using btree(slug);
+        """);
+    }
+}
---
+++ src/Movies.Application/Database/NpgsqlConnectionFactory.cs
@@ -0,0 +1,15 @@
+using System.Data;
+using Npgsql;
+
+namespace Movies.Application.Database;
+
+public class NpgsqlConnectionFactory(string connectionString) : IDbConnectionFactory
+{
+    private readonly string _connectionString = connectionString;
+    public async Task<IDbConnection> CreateConnectionAsync()
+    {
+        var connection = new NpgsqlConnection(_connectionString);
+        await connection.OpenAsync();
+        return connection;
+    }
+}
