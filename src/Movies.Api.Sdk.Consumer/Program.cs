34569382e95edc0974de68a70c6201e8edb2e992
johders <169034808+johders@users.noreply.github.com>
2025-06-09 11:30:53 +0200

    Add remaining endpoints and test with consumer

--- src/Movies.Api.Sdk.Consumer/Program.cs
+++ src/Movies.Api.Sdk.Consumer/Program.cs
@@ -24,7 +24,37 @@ var provider = services.BuildServiceProvider();
 
 var moviesApi = provider.GetRequiredService<IMoviesApi>();
 
-var movie = await moviesApi.GetMovieAsync("crimson-phase-2015");
+//POST create new movie
+// var newMovie = await moviesApi.CreateMovieAsync(new CreateMovieRequest
+// {
+//     Title = "ZJungle book II",
+//     YearOfRelease = 1991,
+//      Genres = new[] { "Adventure", "Drama" }
+// });
+
+//PUT update movie
+// var updatedMovie = await moviesApi.UpdateMovieAsync(Guid.Parse("2ec106a9-bcfa-47f7-8713-5157ae486d54"), new UpdateMovieRequest
+// {
+//     Title = "ZJungler book II",
+//     YearOfRelease = 1993,
+//     Genres = new[] { "Sci-Fi" }
+// });
+
+//DELETE movie
+//await moviesApi.DeleteMovieAsync(Guid.Parse("3e22b621-c331-4aa6-9b25-d60833534d60"));
+
+//GET by slug
+//var movie = await moviesApi.GetMovieAsync("void-memory-2008");
+
+//PUT rate movie
+//await moviesApi.RateMovieAsync(Guid.Parse("30d74bb7-ef56-4e8e-b1e0-373528cbe3d4"), new RateMovieRequest { Rating = 4 });
+
+//GET user ratings
+//var ratings = await moviesApi.GetUserRatingsAsync();
+//Console.WriteLine(JsonSerializer.Serialize(ratings));
+
+//DELETE rating
+//await moviesApi.DeleteRatingAsync(Guid.Parse("8a0db90e-ac53-4c5a-83c8-43aa645abfff"));
 
 var request = new GetAllMoviesRequest
 {
@@ -35,6 +65,7 @@ var request = new GetAllMoviesRequest
     PageSize = 4
 };
 
+//GET all movies
 var movies = await moviesApi.GetMoviesAsync(request);
 
 foreach (var item in movies.Items)
@@ -43,5 +74,5 @@ foreach (var item in movies.Items)
 }
 
 Console.WriteLine("--------------------------------------");
-Console.WriteLine(JsonSerializer.Serialize(movie));
+//Console.WriteLine(JsonSerializer.Serialize(movie));
 Console.ReadLine();
