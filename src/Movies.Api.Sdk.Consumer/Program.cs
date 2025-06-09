17aa5f8c85f2e1a017b2f622492b9400abfe35e0
johders <169034808+johders@users.noreply.github.com>
2025-06-09 08:36:46 +0200

    Integrate refit and expose interface for consumer

--- src/Movies.Api.Sdk.Consumer/Program.cs
+++ src/Movies.Api.Sdk.Consumer/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Text.Json;
+using Movies.Api.Sdk;
+using Refit;
+
+var moviesApi = RestService.For<IMoviesApi>("http://localhost:5296");
+
+var movie = await moviesApi.GetMovieAsync("crimson-phase-2015");
+
+Console.WriteLine(JsonSerializer.Serialize(movie));
+Console.ReadLine();
