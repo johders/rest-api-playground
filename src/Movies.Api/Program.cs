d701518f4ddf9884e697230d4c73aecb9083c40e
johders <169034808+johders@users.noreply.github.com>
2025-06-09 12:04:15 +0200

    Set up minimal api endpoint structure

--- src/Movies.Api/Program.cs
+++ src/Movies.Api/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Movies.Api.Auth;
+using Movies.Api.Endpoints;
 using Movies.Api.Health;
 using Movies.Api.Mapping;
 using Movies.Api.Swagger;
@@ -70,7 +71,7 @@ builder.Services.AddOutputCache(x =>
     });
 });
 
-builder.Services.AddControllers();
+//builder.Services.AddControllers();
 
 builder.Services.AddHealthChecks().AddCheck<DatabaseHealthCheck>(DatabaseHealthCheck.Name);
 
@@ -106,7 +107,8 @@ app.UseAuthorization();
 app.UseOutputCache();
 
 app.UseMiddleware<ValidationMappingMiddleware>();
-app.MapControllers();
+//app.MapControllers();
+app.MapApiEndpoints();
 
 var dbInitializer = app.Services.GetRequiredService<DbInitializer>();
 await dbInitializer.InitializeAsync();
