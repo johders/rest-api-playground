f5a71ed7f6b09b1efa738ba62025ae39183e9ced
johders <169034808+johders@users.noreply.github.com>
2025-06-09 10:47:26 +0200

    Add dynamic token injection using authtokenprovider

--- src/Movies.Api.Sdk.Consumer/Program.cs
+++ src/Movies.Api.Sdk.Consumer/Program.cs
@@ -2,6 +2,7 @@
 using System.Text.Json;
 using Microsoft.Extensions.DependencyInjection;
 using Movies.Api.Sdk;
+using Movies.Api.Sdk.Consumer;
 using Movies.Contracts.Requests;
 using Refit;
 
@@ -9,11 +10,15 @@ using Refit;
 
 var services = new ServiceCollection();
 
-services.AddRefitClient<IMoviesApi>(x => new RefitSettings
-    {
-        AuthorizationHeaderValueGetter = (request, token) => Task.FromResult("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3YjRiMjczMS1lMjlmLTRmNDMtOTE5Yi1jOGUzZDdkM2Q1MzkiLCJzdWIiOiJuaWNrQG5pY2tjaGFwc2FzLmNvbSIsImVtYWlsIjoibmlja0BuaWNrY2hhcHNhcy5jb20iLCJ1c2VyaWQiOiJkODU2NmRlMy1iMWE2LTRhOWItYjg0Mi04ZTM4ODdhODJlNDEiLCJhZG1pbiI6dHJ1ZSwidHJ1c3RlZF9tZW1iZXIiOnRydWUsIm5iZiI6MTc0OTQ0NzA4NiwiZXhwIjoxNzQ5NDc1ODg2LCJpYXQiOjE3NDk0NDcwODYsImlzcyI6Imh0dHBzOi8vaWQudGVzdGVyLmNvbSIsImF1ZCI6Imh0dHBzOi8vbW92aWVzLnRlc3Rlci5jb20ifQ.505pRM5q2RQ4WHUXwJRNi9OvOYgJ33jEUfeyPKlkbLs")
-    })
-    .ConfigureHttpClient(x => x.BaseAddress = new Uri("http://localhost:5296"));
+services
+    .AddHttpClient()
+    .AddSingleton<AuthTokenProvider>()
+    .AddRefitClient<IMoviesApi>(s => new RefitSettings
+        {
+            AuthorizationHeaderValueGetter = async (request, token) => await s.GetRequiredService<AuthTokenProvider>().GetTokenAsync()
+        })
+        .ConfigureHttpClient(x =>
+            x.BaseAddress = new Uri("http://localhost:5296"));
 
 var provider = services.BuildServiceProvider();
 
