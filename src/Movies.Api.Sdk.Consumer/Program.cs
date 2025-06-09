2c51dfc29f726532601855d923689a21081feb6e
johders <169034808+johders@users.noreply.github.com>
2025-06-09 08:48:32 +0200

    Add getall endpoint to sdk and test usage in consumer

--- src/Movies.Api.Sdk.Consumer/Program.cs
+++ src/Movies.Api.Sdk.Consumer/Program.cs
@@ -1,11 +1,29 @@
 ﻿
 using System.Text.Json;
 using Movies.Api.Sdk;
+using Movies.Contracts.Requests;
 using Refit;
 
 var moviesApi = RestService.For<IMoviesApi>("http://localhost:5296");
 
 var movie = await moviesApi.GetMovieAsync("crimson-phase-2015");
 
+var request = new GetAllMoviesRequest
+{
+    Title = null,
+    YearOfRelease = null,
+    SortBy = "-title",
+    Page = 1,
+    PageSize = 4
+};
+
+var movies = await moviesApi.GetMoviesAsync(request);
+
+foreach (var item in movies.Items)
+{
+    Console.WriteLine(JsonSerializer.Serialize(item));
+}
+
+Console.WriteLine("--------------------------------------");
 Console.WriteLine(JsonSerializer.Serialize(movie));
 Console.ReadLine();
