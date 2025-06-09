ef2d6dca06785fbec42a8d05f23a42c5ca2637b8
johders <169034808+johders@users.noreply.github.com>
2025-06-09 09:08:04 +0200

    Use httpclientfactory with refit via di

--- src/Movies.Api.Sdk.Consumer/Program.cs
+++ src/Movies.Api.Sdk.Consumer/Program.cs
@@ -1,10 +1,19 @@
 ﻿
 using System.Text.Json;
+using Microsoft.Extensions.DependencyInjection;
 using Movies.Api.Sdk;
 using Movies.Contracts.Requests;
 using Refit;
 
-var moviesApi = RestService.For<IMoviesApi>("http://localhost:5296");
+//var moviesApi = RestService.For<IMoviesApi>("http://localhost:5296");
+
+var services = new ServiceCollection();
+
+services.AddRefitClient<IMoviesApi>().ConfigureHttpClient(x => x.BaseAddress = new Uri("http://localhost:5296"));
+
+var provider = services.BuildServiceProvider();
+
+var moviesApi = provider.GetRequiredService<IMoviesApi>();
 
 var movie = await moviesApi.GetMovieAsync("crimson-phase-2015");
 
